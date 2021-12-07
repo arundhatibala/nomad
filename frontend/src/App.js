e117507003790871300eaada4cb69e937d4c22b9
arundhati08 <write2arundhati@gmail.com>
2021-12-07 23:36:22 +0530

    screen routes created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,27 @@
-import React from "react"
-import { Container } from "react-bootstrap"
-import Header from "./Components/Header"
-import Footer from "./Components/Footer";
+import React from 'react'
 import background from "./background.png"
-function App() {
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from '../../frontend/src/Components/Header'
+import Footer from '../../frontend/src/Components/Footer'
+import Home from './Screens/Home'
+
+const App = () => {
   return (
     <div style={{ backgroundImage: `url(${background})` }}>
+    <Router>
     <Header />
     <main>
       <Container>
-      <h1>Welcome to nomad.</h1>
-      </Container>
-      </main>
+      <Routes>
+      <Route path='/' element={<Home />} />
+       </Routes>
+    </Container>
+    </main> 
     <Footer />
-</div>
-  );
+    </Router>
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/Screens/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Card } from 'react-bootstrap' 
+const Home = () => {
+    return (
+        <>
+        <h2>Welcome to nomad.</h2>    
+        </>
+    )
+}
+
+export default Home
