fc69f1d4d73889de9083fcb1523ef03c6b475528
arundhati08 <write2arundhati@gmail.com>
2021-12-08 00:08:58 +0530

    home.js modified

--- frontend/src/Screens/Home.js
+++ frontend/src/Screens/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import { Card } from 'react-bootstrap' 
+import Fade from 'react-reveal/Fade';
 const Home = () => {
     return (
         <>
-        <h2>Welcome to nomad.</h2>    
+        <center>  
+        <Fade cascade duration={4000}>
+        <h1 style ={{fontSize:"180px"}}>nomad.</h1>
+        <br></br>
+        <h2>Belong anywhere.</h2>
+        </Fade>
+        </center>
+        <Card className ="py-3" style = {{marginTop: "50px"}}>
+            <center><Card.Title>Destinations</Card.Title></center>
+        </Card>
         </>
     )
 }
