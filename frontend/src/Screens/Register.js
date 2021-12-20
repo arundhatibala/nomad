ffa3ad5bef3bcc36a19f73cbf2f4337c78ac771d
Siddh Veer Bakshi <siddh.bakshi_ug23@ashoka.edu.in>
2021-12-20 22:06:34 +0530

    FormContainer, Loader, Message added as components and registerscreen edited.

--- frontend/src/Screens/Register.js
+++ frontend/src/Screens/Register.js
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import FormContainer from '../Components/FormContainer'
+import Message from '../Components/Message'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from '../Components/Loader'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+// import { register } from '../actions/userActions'
+
 
 const Register = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [message, setMessage] = useState(null)
+
+    const dispatch = useDispatch()
+
+    
+    const userRegister = useSelector(state => state.userRegister)
+    const { loading, error, userInfo, success } = userRegister
+
+    let navigate = useNavigate()
+    let location = useLocation()
+
+    // const submitHandler = (e) => {
+    //     e.preventDefault()
+    //     if (password !== confirmPassword) {
+    //         setMessage('Passwords do not match')
+    //     }
+    //     else {
+    //         dispatch(register(name, email, password))
+    //         { success && navigate('/cart') }
+    //     }
+    // }
     return (
-        <div>
-            
-        </div>
+        <FormContainer>
+            <h1>Sign Up</h1>
+            {message && <Message variant='danger'>{message}</Message>}
+            {error && <Message variant='danger'>{error}</Message>}
+            {loading && <Loader />}
+
+        </FormContainer>
     )
 }
 
