663b0f0d62d0fe8ba8b730167ace4da15c0df4a2
Siddh Veer Bakshi <siddh.bakshi_ug23@ashoka.edu.in>
2021-12-22 13:19:21 +0530

    registerscreen frontend done

--- frontend/src/Screens/Register.js
+++ frontend/src/Screens/Register.js
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import Loader from '../Components/Loader'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 // import { register } from '../actions/userActions'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+
 
 
 const Register = () => {
@@ -14,14 +16,14 @@ const Register = () => {
     const [confirmPassword, setConfirmPassword] = useState('')
     const [message, setMessage] = useState(null)
 
-    const dispatch = useDispatch()
+    // const dispatch = useDispatch()
 
     
-    const userRegister = useSelector(state => state.userRegister)
-    const { loading, error, userInfo, success } = userRegister
+    // const userRegister = useSelector(state => state.userRegister)
+    // const { loading, error, userInfo, success } = userRegister
 
-    let navigate = useNavigate()
-    let location = useLocation()
+    // let navigate = useNavigate()
+    // let location = useLocation()
 
     // const submitHandler = (e) => {
     //     e.preventDefault()
@@ -36,10 +38,43 @@ const Register = () => {
     return (
         <FormContainer>
             <h1>Sign Up</h1>
-            {message && <Message variant='danger'>{message}</Message>}
+            {/* {message && <Message variant='danger'>{message}</Message>}
             {error && <Message variant='danger'>{error}</Message>}
-            {loading && <Loader />}
-
+            {loading && <Loader />} */}
+            {/* <Form onSubmit={submitHandler}> */}
+            <Form.Group controlId='name'>
+                <Form.Label>Name</Form.Label>
+                <Form.Control type='name' placeholder='Enter name' value={name} onChange={(e) => setName(e.target.value)}>
+                </Form.Control>
+            </Form.Group>
+            <br></br>
+            <Form.Group controlId='email'>
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control type='email' placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)}>
+                </Form.Control>
+            </Form.Group>
+            <br></br>
+            <Form.Group controlId='password'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control type='password' placeholder='Enter password' value={password} onChange={(e) => setPassword(e.target.value)}>
+                </Form.Control>
+            </Form.Group>
+            <br></br>
+            <Form.Group controlId='ConfirmPassword'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control type='password' placeholder='Confirm password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}>
+                </Form.Control>
+            </Form.Group>
+            <br></br>
+            <Button type='submit' variant='secondary'>
+                Sign Up
+            </Button>
+            {/* </Form> */}
+            <Row className='py-3'>
+                <Col>
+                    {/* Have an Account? <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>Login</Link> */}
+                </Col>
+            </Row>
         </FormContainer>
     )
 }
