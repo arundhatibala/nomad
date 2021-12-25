80f8c3846b1d20395c2e6f59ffd1515e9cc99f29
Siddh Veer Bakshi <siddh.bakshi_ug23@ashoka.edu.in>
2021-12-25 15:41:19 +0530

    updated userReducer, login.js and register.js

--- frontend/src/Screens/Login.js
+++ frontend/src/Screens/Login.js
@@ -5,34 +5,48 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../Components/Message'
 import Loader from '../Components/Loader'
 import FormContainer from '../Components/FormContainer'
-// import { login } from '../actions/userActions'
-// import { USER_LOGIN_SUCCESS } from '../constants/userConstants'
+import  { login } from '../actions/userActions'
+import { USER_LOGIN_SUCCESS } from '../constants/userConstants'
 import axios from 'axios'
 // import Google from '../google.png'
 
 
-const Login = () => {
+const Login = ({ }) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const dispatch = useDispatch()
-    const userLogin = useSelector(state => state.userLogin)
-    // const { loading, error, userInfo, success } = userLogin
+    const userLogin = useSelector(state => state.userLogin || {} )
+    const { loading, error, userInfo, success } = userLogin  
     const [data, setData] = useState("")
 
     let navigate = useNavigate()
     let location = useLocation()
+
     const redirect = location.search ? location.search.split('=')[1] : '/'
 
+    useEffect(async () => {
+        if(userInfo)
+        {
+            navigate(redirect)
+        }
+        const callBack = await axios.get("api/users/auth/google")
+        setData(callBack.data.loginLink) 
+        console.log(data)
+    },[userInfo, useNavigate, redirect, data])
 
-    
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch(login(email, password))
+        {success && navigate('/cart')}
+    }
     return (
         
         <FormContainer>
             <center><h1>Sign In</h1></center>
-            {/* {error && <Message variant='danger'>{error}</Message>}
-            {loading && <Loader />} */}
+            {error && <Message variant='danger'>{error}</Message>}
+            {loading && <Loader />}
             
-            {/* <Form onSubmit={submitHandler}> */}
+            <Form onSubmit={submitHandler}>
                 <Form.Group controlId='email'>
                     <Form.Label>Email Address</Form.Label>
                     <Form.Control type='email' placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)}>
@@ -48,10 +62,10 @@ const Login = () => {
                 <Button type='submit' variant='secondary'>
                     Sign In
                 </Button>
-                {/* </Form> */}
+                </Form>
             <Row className='py-3'>
                 <Col>
-                {/* New Customer? <Link to={redirect ? `/register?redirect=${redirect}`:'/register'}>Register</Link> */}
+                New Customer? <Link to={redirect ? `/register?redirect=${redirect}`:'/register'}>Register</Link>
                 </Col>
             </Row>
         </FormContainer>
--- frontend/src/Screens/Register.js
+++ frontend/src/Screens/Register.js
@@ -1,48 +1,55 @@
+//To register a new user
 import React, { useState, useEffect } from 'react'
-import FormContainer from '../Components/FormContainer'
-import Message from '../Components/Message'
-import { useDispatch, useSelector } from 'react-redux'
-import Loader from '../Components/Loader'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
-// import { register } from '../actions/userActions'
 import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../Components/Message'
+import Loader from '../Components/Loader'
+import FormContainer from '../Components/FormContainer'
+import { register } from '../actions/userActions'
+import { USER_LOGIN_SUCCESS } from '../constants/userConstants'
 
-
-
-const Register = () => {
+const Register = ({ }) => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
     const [message, setMessage] = useState(null)
 
-    // const dispatch = useDispatch()
-
+    const dispatch = useDispatch()
     
-    // const userRegister = useSelector(state => state.userRegister)
-    // const { loading, error, userInfo, success } = userRegister
+    const userRegister = useSelector(state => state.userRegister || {})
+    const { loading, error, userInfo, success } = userRegister
 
-    // let navigate = useNavigate()
-    // let location = useLocation()
+    let navigate = useNavigate()
+    let location = useLocation()
 
-    // const submitHandler = (e) => {
-    //     e.preventDefault()
-    //     if (password !== confirmPassword) {
-    //         setMessage('Passwords do not match')
-    //     }
-    //     else {
-    //         dispatch(register(name, email, password))
-    //         { success && navigate('/cart') }
-    //     }
-    // }
+    const redirect = location.search ? location.search.split('=')[1] : '/'
+    
+    useEffect(() => {
+        if (userInfo) {
+            navigate(redirect)
+        }
+    }, [userInfo, useNavigate, redirect])
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        if (password !== confirmPassword) {
+            setMessage('Passwords do not match')
+        }
+        else {
+            dispatch(register(name, email, password))
+            { success && navigate('/cart') }
+        }
+    }
     return (
         <FormContainer>
-            <br></br>
             <h1>Sign Up</h1>
-            {/* {message && <Message variant='danger'>{message}</Message>}
+            <br></br>
+            {message && <Message variant='danger'>{message}</Message>}
             {error && <Message variant='danger'>{error}</Message>}
-            {loading && <Loader />} */}
-            {/* <Form onSubmit={submitHandler}> */}
+            {loading && <Loader />}
+            <Form onSubmit={submitHandler}>
             <Form.Group controlId='name'>
                 <Form.Label>Name</Form.Label>
                 <Form.Control type='name' placeholder='Enter name' value={name} onChange={(e) => setName(e.target.value)}>
@@ -70,10 +77,10 @@ const Register = () => {
             <Button type='submit' variant='secondary'>
                 Sign Up
             </Button>
-            {/* </Form> */}
+            </Form>
             <Row className='py-3'>
                 <Col>
-                    {/* Have an Account? <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>Login</Link> */}
+                    Have an Account? <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>Login</Link>
                 </Col>
             </Row>
         </FormContainer>
